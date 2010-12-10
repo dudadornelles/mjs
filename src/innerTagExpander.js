31462d3399a79e3e4372a80d7313db828f53e053
Duda Dornelles <ddornell@thougthworks.com>
2010-12-09 22:08:36 -0600

    wqextracting some methods in innerTagsExpander

--- src/innerTagExpander.js
+++ src/innerTagExpander.js
@@ -1,14 +1,25 @@
+
 InnerTagExpander = function() {
+
+
+	var skipAttributes = function(tags) {
+		if(tags && typeof(tags[0]) === 'object')
+			return tags.slice(1, tags.lenght);
+		return tags;
+	};
+
+	var expandTags = function(tags) {
+		var expandedTags = "";
+		for(var i = 0; i < tags.length; i++) {
+			expandedTags += (typeof(tags[i]) === 'function' ? tags[i]() : tags[i]) || "";
+		}
+		return expandedTags;
+	};
+
 	return {
 		expand : function(tags) {
-			if(tags && typeof(tags[0]) === 'object') {
-				tags = tags.slice(1, tags.lenght);
-			}
-			var expandedTags = "";
-			for(var i = 0; i < tags.length; i++) {
-				expandedTags += (typeof(tags[i]) === 'function' ? tags[i]() : tags[i]) || "";
-			}
-			return expandedTags;
+			return expandTags(skipAttributes(tags));
 		}
 	}
 }
+
